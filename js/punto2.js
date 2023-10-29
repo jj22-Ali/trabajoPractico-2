66c9702586d21c6b30f86f5d2e53526484cd967e
Juan Jose <juanjosealifigueroa56@gmail.com>
2023-10-29 17:53:32 -0300

    Resuelto el ejercicio 2

---
+++ js/punto2.js
@@ -0,0 +1,28 @@
+alert("Ejercicio 2");
+
+let nota = parseInt(prompt("Ingrese la nota del examen:"))
+
+if(nota >= 0 && nota <= 2){
+    alert("nota: Muy deficiente");
+}else {
+
+    if (nota >= 3 && nota <=4){
+        alert("nota: Insuficiente");
+    } else {
+        if (nota >= 5 && nota <=6){
+            alert("nota: suficiente");
+        } else {
+            if (nota === 7 ){
+                alert("nota: Bien");
+            } else {
+                if (nota >= 8 && nota <=9){
+                    alert("nota: Notable");
+                } else {
+                    if (nota === 10){
+                        alert("nota: Sobresaliente");
+                    }
+                }
+            }
+        }
+    }
+} 
