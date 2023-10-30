bd0a7c81d4afb6122a5ef7ab4de53a14f4e3ade0
Juan Jose <juanjosealifigueroa56@gmail.com>
2023-10-30 00:07:01 -0300

    terminado el punto 7

---
+++ js/punto7.js
@@ -0,0 +1,37 @@
+alert("Ejercicio 7")
+
+let numeroPiramide;
+let i,j;
+
+do{
+
+    numeroPiramide = parseInt(prompt("Ingrese hasta que numero quiere su piramide: "));
+
+    if(numeroPiramide < 50){
+
+    
+        for(i = numeroPiramide ; i >= 1 ; i--){
+    
+            for(j = 1 ; j <= i; j++){
+    
+                document.write(i)
+    
+            }
+        
+            document.write("<br>");
+    
+        }
+        
+        break;
+    
+    }else{
+    
+        alert("Porfavor ingrese numeros menores a 50");
+    
+    }
+
+}while(confirm("¿Quieres intentarlo otra vez?"))
+
+
+
+
