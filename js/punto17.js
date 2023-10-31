9176ad5d284769434f46c1c3c2ac4cb258654d29
Juan Jose <juanjosealifigueroa56@gmail.com>
2023-10-30 23:08:31 -0300

    terminado el punto 17

---
+++ js/punto17.js
@@ -0,0 +1,17 @@
+    alert("Ejercicio 17")
+
+    let frase = prompt("Ingrese una frase: ");
+    let i, posicionVocal, vocal;
+
+    for(i = 0; i <= frase.length - 1; i++){
+
+        if(frase[i] == "a" || frase[i] == "e" || frase[i] == "i" || frase[i] == "o" || frase[i] == "u" ){
+
+            vocal = frase[i];
+            posicionVocal = i;
+            break;
+        }
+
+    }
+
+    console.log("La vocal " + vocal + " está en la posición " + posicionVocal);
